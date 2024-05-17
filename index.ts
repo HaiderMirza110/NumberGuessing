ace4c5d4cc0d36d86e891f675c99c5292d605116
HaiderMirza110 <163348029+HaiderMirza110@users.noreply.github.com>
2024-05-18 00:42:47 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+const randomNumber = 7;
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([ 
+{name: "random", type: "number", message: "Enter your Guess Number between 0 to 10"}
+])
+
+if (answer.random === randomNumber)     {console.log("Your Guess is Correct")}
+  else {console.log("Your Guess is Incorrect")}
